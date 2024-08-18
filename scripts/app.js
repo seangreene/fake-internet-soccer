35e4dfe61dc9f73002a679a5ce82f3389d2bf900
Sean Greene <seangreene89@gmail.com>
2024-08-17 22:30:09 -0700

    add chart and stuf

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,10 @@ import BumpChart from './components/BumpChart.svelte';
 async function main() {
   // the first parameter should be the path to the file
   // the second parameter *must* be “import.meta.url”
-  const url = new URL('../_data/standings-xii.json', import.meta.url);
-
-  // Call it in
-  const data = await json(url);
-
-  const chartContainer = document.getElementById('bump');
-
-  renderSvelteComponent(BumpChart, chartContainer, { data: data });
+  // const url = new URL('../_data/standings-xii.json', import.meta.url);
+  // // Call it in
+  // const data = await json(url);
+  // const chartContainer = document.getElementById('bump');
+  // renderSvelteComponent(BumpChart, chartContainer, { data: data });
 }
 main().catch(console.error);
