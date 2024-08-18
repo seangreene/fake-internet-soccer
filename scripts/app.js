3330b093b82584cd1468f9e3baecee31234e6918
Sean Greene <seangreene89@gmail.com>
2024-08-17 23:21:52 -0700

    idk

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,2 @@
-// import { json } from 'd3-fetch';
-// import { renderSvelteComponent } from './utils/render.js';
-// import BumpChart from './components/BumpChart.svelte';
-
-async function main() {
-  // the first parameter should be the path to the file
-  // the second parameter *must* be “import.meta.url”
-  // const url = new URL('../_data/standings-xii.json', import.meta.url);
-  // // Call it in
-  // const data = await json(url);
-  // const chartContainer = document.getElementById('bump');
-  // renderSvelteComponent(BumpChart, chartContainer, { data: data });
-}
+async function main() {}
 main().catch(console.error);
